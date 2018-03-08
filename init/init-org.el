dfa2befe2972f6c0303bffcec338f231316782f4
Serhii Pererva <sergey.pererva@gmail.com>
2018-03-08 22:16:34 +0200

    Staging folder for mobile-org

--- init/init-org.el
+++ init/init-org.el
@@ -1,11 +1,12 @@
 (require 'use-package)
+(require 'org-mobile)
 (require 'functions)
 
 (use-package org
   :ensure t
   :init
-  (setq org-default-notes-file
-        (convert-standard-filename "~/org/notes.org"))
+  (setq org-default-notes-file (convert-standard-filename "~/org/notes.org")
+        org-mobile-directory   (convert-standard-filename "~/org/mobile"))
   :bind (("C->" . serhiip--take-notes-from-region)
          ("C-!" . serhiip--take-note-todo)))
 
