dc4b8de3a00fc8bb3375c7b9862fc86b385bd933
Serhii Pererva <sergey.pererva@gmail.com>
2018-03-13 22:26:46 +0200

    Better way of capturing log edit related buffer names

--- init/init-magit.el
+++ init/init-magit.el
@@ -3,7 +3,7 @@
 (use-package magit
   :ensure t
   :init
-  (add-to-list 'auto-mode-alist '("\\COMMIT_EDITMSG\\'" . magit-log))
+  (add-to-list 'auto-mode-alist '(".*_EDITMSG\\'" . magit-log))
   :bind
   (("C-x g" . 'magit-status)))
 
