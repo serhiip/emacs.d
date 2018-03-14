930d2b59c227e0a9c1f1bddbc979e2ef503df5af
Serhii Pererva <sergey.pererva@gmail.com>
2018-03-14 22:26:42 +0200

    Set default navigation system to helm in projectile

--- init/init-projectile.el
+++ init/init-projectile.el
@@ -3,7 +3,10 @@
 (use-package projectile
   :demand
   :ensure t
-  :init   (setq projectile-use-git-grep t)
+  :init
+  (setq projectile-use-git-grep t)
+  (custom-set-variables
+   '(projectile-completion-system 'helm))
   :config (projectile-global-mode t))
 
 (provide 'init-projectile)
