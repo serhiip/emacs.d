f491549461cee68047c7de5db540725f74d34c75
Serhii Pererva <sergey.pererva@gmail.com>
2018-03-15 11:05:21 +0200

    Start a flycheck mode automatically in python mode

--- init/init-python.el
+++ init/init-python.el
@@ -4,7 +4,8 @@
   :ensure t
   :init
   (with-eval-after-load 'flycheck
-    (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup))
+    (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup)
+    (add-hook 'python-mode-hook #'flycheck-mode))
   :ensure-system-package (pylint . "pip install pylint"))
 
 (provide 'init-python)
