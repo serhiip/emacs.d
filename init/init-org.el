05df07223ea68f97ba328a69c747376917f66acc
Serhii Pererva <sergey.pererva@gmail.com>
2018-03-20 22:43:08 +0200

    More fixes related to org mostly

--- init/init-org.el
+++ init/init-org.el
@@ -2,11 +2,25 @@
 (require 'org-mobile)
 (require 'functions)
 
+(defvar org-server-location)
+(defvar mobile-dir-truename)
+
+(defun serhiip--push-notes-to-server ()
+  (serhiip-rsync mobile-dir-truename org-server-location))
+
+(defun serhiip--pull-mobileorg-file ()
+  (let ((from (concat org-server-location org-mobile-capture-file)))
+    (serhiip-rsync from mobile-dir-truename)))
+
+(defun serhiip--push-mobileorg-file ()
+  (let ((from (concat mobile-dir-truename org-mobile-capture-file)))
+    (serhiip-rsync from org-server-location)))
+
 (use-package org
   :ensure t
   :init
-  (setq org-default-notes-file (serhiip--org-file-path "/gtd.org")
-        org-mobile-directory   (serhiip--org-file-path "/mobile")
+  (setq org-default-notes-file (serhiip-org-file-path "/gtd.org")
+        org-mobile-directory   (serhiip-org-file-path "/mobile")
         org-server-location    "org-server:/home/serhii/org-sync/org/"
         mobile-dir-truename    (file-truename (concat org-mobile-directory "/"))
         org-todo-keywords      '((sequence "TODO" "WAIT"     "|" "DONE")
@@ -20,9 +34,9 @@
                                   "** TOBUY %i %?\n  %U\n  %a")
                                  ("r" "Add to reading list" entry (file+headline org-default-notes-file "to read")
                                   "** TOREAD %i %?\n  %U\n  %a")
-                                 ("f" "New feature" entry (file (serhiip--org-get-current-file))
+                                 ("f" "New feature" entry (file (serhiip-org-get-current-file))
                                   "* FEATURE %i %?\n  %U\n  %a")
-                                 ("b" "Bug" entry (file (serhiip--org-get-current-file))
+                                 ("b" "Bug" entry (file (serhiip-org-get-current-file))
                                   "* BUG %i %?\n  %U\n  %a"))
         org-todo-keyword-faces '(("TODO" . "red")
                                  ("DOING" . "yellow")
@@ -31,17 +45,9 @@
                                  ("TOREAD" . "dark cyan")
                                  ("TOBUY" . "wheat"))
         org-ellipsis           " ⤵")
-  (add-hook
-   'org-mobile-post-push-hook
-   #'(lambda () (serhiip--rsync mobile-dir-truename org-server-location)))
-  (add-hook
-   'org-mobile-pre-pull-hook
-   #'(lambda () (let ((from (concat org-server-location org-mobile-capture-file)))
-             (serhiip--rsync from mobile-dir-truename))))
-  (add-hook
-   'org-mobile-post-pull-hook
-   #'(lambda () (let ((from (concat mobile-dir-truename org-mobile-capture-file)))
-             (serhiip--rsync from org-server-location))))
+  (add-hook 'org-mobile-post-push-hook #'serhiip--push-notes-to-server)
+  (add-hook 'org-mobile-pre-pull-hook #'serhiip--pull-mobileorg-file)
+  (add-hook 'org-mobile-post-pull-hook #'serhiip--push-mobileorg-file)
   :bind (("C->"   . serhiip-take-notes-from-region)
          ("C-c c" . serhiip-take-note-todo)))
 
