c1ad2a727fedfda5af1270cf28c4bf564f6538b8
Serhii Pererva <sergey.pererva@gmail.com>
2018-04-02 15:15:13 +0300

    Docstrings for projectile init file

--- init/init-projectile.el
+++ init/init-projectile.el
@@ -1,4 +1,11 @@
+;;; init-projectile.el --- Common functions -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'use-package)
+(require 'projectile)
 
 (use-package projectile
   :demand
@@ -7,6 +14,8 @@
   (setq projectile-use-git-grep t)
   (custom-set-variables
    '(projectile-completion-system 'helm))
-  :config (projectile-global-mode t))
+  :config (projectile-mode t))
 
 (provide 'init-projectile)
+
+;;; init-projectile.el ends here
