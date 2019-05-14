7fe364a645ee58ad8885e41298bc6aeb13a82980
Serhii Pererva <serhii.pererva@gmail.com>
2019-05-14 22:47:19 +0100

    Remve org-mobile as I no longer use it

--- init/init-org.el
+++ init/init-org.el
@@ -7,39 +7,19 @@
 ;;; Code:
 
 (require 'use-package)
-(require 'org-mobile)
 (require 'functions)
-
-(defvar org-server-location)
-(defvar mobile-dir-truename)
-
-(defun serhiip--push-notes-to-server ()
-  "Perform rsync for org files to send the to server."
-  (serhiip-rsync mobile-dir-truename org-server-location))
-
-(defun serhiip--pull-mobileorg-file ()
-  "Perform rsync from server to local sync folder."
-  (let ((from (concat org-server-location org-mobile-capture-file)))
-    (serhiip-rsync from mobile-dir-truename)))
-
-(defun serhiip--push-mobileorg-file ()
-  "Perform rsync of mobileorg file."
-  (let ((from (concat mobile-dir-truename org-mobile-capture-file)))
-    (serhiip-rsync from org-server-location)))
+(require 'org-id)
 
 (use-package org
   :ensure t
   :init
   (setq org-default-notes-file (serhiip-org-file-path "/gtd.org")
-        org-mobile-directory   (serhiip-org-file-path "/mobile")
-        org-server-location    "org-server:/home/serhii/org-sync/org/"
-        mobile-dir-truename    (file-truename (concat org-mobile-directory "/"))
         org-todo-keywords      '((sequence "TODO" "WAIT"     "|" "DONE")
                                  (sequence "TOBUY"           "|" "DONE")
                                  (sequence "TOREAD"          "|" "DONE")
                                  (sequence "BUG"             "|" "FIXED")
                                  (sequence "FEATURE" "DOING" "|" "DONE")
-                                 (sequence "MEMO"          "|" "DONE"))
+                                 (sequence "MEMO"            "|" "DONE"))
         org-capture-templates  '(("t" "To Do" entry
                                   (file+headline org-default-notes-file "to do")
                                   "** TODO %i %?\n  %a")
@@ -66,9 +46,6 @@
                                  ("TOREAD"  . "dark cyan")
                                  ("TOBUY"   . "wheat"))
         org-ellipsis           " ⤵")
-  (add-hook 'org-mobile-post-push-hook #'serhiip--push-notes-to-server)
-  (add-hook 'org-mobile-pre-pull-hook #'serhiip--pull-mobileorg-file)
-  (add-hook 'org-mobile-post-pull-hook #'serhiip--push-mobileorg-file)
   :bind (("C->"   . serhiip-take-notes-from-region)
          ("C-c c" . serhiip-take-note-todo)))
 
