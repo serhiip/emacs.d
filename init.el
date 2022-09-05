90111dda15a2cfd09e9832b8887427d5c2edfb54
Serhii Pererva <serhii@serhiis-mbp-2.mynet>
2022-09-05 21:18:40 +0100

    add markdown preview

--- init.el
+++ init.el
@@ -441,6 +441,16 @@
 (leaf protobuf-mode
   :ensure t)
 
+(leaf impatient-mode
+  :ensure t)
+
+;; impatient mode
+;; https://stackoverflow.com/a/36189456
+(defun markdown-html (buffer)
+    (princ (with-current-buffer buffer
+      (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://ndossougbe.github.io/strapdown/dist/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
+           (current-buffer)))
+
 (org-todo-list)
 
 ;;; init.el ends here
